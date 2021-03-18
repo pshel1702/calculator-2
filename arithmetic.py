a9dbafc5df6a1c28d4d2dfa9fe2f2c414e889f38
vagrant <vagrant@vagrant.vm>
2021-03-18 04:40:52 +0000

    Modified input to be list

--- arithmetic.py
+++ arithmetic.py
@@ -1,86 +1,115 @@
 """Functions for common math operations."""
 
+def convert_to_int(input_list):
+    num=[]
+    for i in range(1,len(input_list)):
+        num.append(int(input_list[i]))
+    
+    return num
 
-def add(num1, num2):
+
+def add(user_expression):
     """Return the sum of the two inputs."""
     # define a variable "sum"
     # set this variable as the sum of num1, num2. sum = num1+num2
     # return sum
-    return num1 + num2
+    sum = 0
+    num = convert_to_int(user_expression)
+    for i in range (0,len(num)):
+        sum = sum + num[i]
+    return sum
 
-def subtract(num1, num2):
+def subtract(user_expression):
     """Return the second number subtracted from the first."""
     #def variable for subtract
     #set subtract variable to find difference between two numbers
     #return subtract variable
-    return num1 - num2
+    
+    num = convert_to_int(user_expression)
+    difference = num[0]
+    for i in range (1,len(num)):
+        difference = difference-num[i]
+    return difference
 
 
-def multiply(num1, num2):
+def multiply(user_expression):
     """Multiply the two inputs together."""
     #define variable for result
     #save result of num1 multiplied by num2
     #return result
 
-    return num1 * num2
+    num = convert_to_int(user_expression)
+    product = 1
+    for i in range (0,len(num)):
+        product = product * num[i]
+    return product
 
-def divide(num1, num2):
+def divide(user_expression):
     """Divide the first input by the second and return the result."""
     #define function for divide
     #add quotient variable that divides num1 and num2
     #return quotient
+    num = convert_to_int(user_expression)
+    print(num)
+    return num[0]/num[1]
 
-    return num1/num2
 
-
-def square(num1):
+def square(user_expression):
     """Return the square of the input."""
     #define variable for square
     #save result of squaring to the variable
     #return result [squared]
+    num = convert_to_int(user_expression)
+    return num[0] ** 2
 
-    return power(num1,2) 
-
-def cube(num1):
+def cube(user_expression):
     """Return the cube of the input."""
 
     #define variable for a cube
     #take num1 to the third power
     #return the cube variable
-    
-    return power(num1,3)
+    num = convert_to_int(user_expression)
+    return num[0]**3
 
 
-def power(num1, num2):
+def power(user_expression):
     """Raise num1 to the power of num2 and return the value."""
     #define variable for result
     #save result of num1 raised to num2 into result
     #return result
-    return num1 ** num2
 
-def mod(num1, num2):
+    num = convert_to_int(user_expression)
+    return num[0] ** num[1]
+
+def mod(user_expression):
     """Return the remainder of num1 / num2."""
 
     #define remainder variable
     #capturn remainder when num1 / num2
     #return remainder variable
-    return num1 % num2
+
+    num = convert_to_int(user_expression)
+    return num[0] % num[1]
 
 
 # define function add_mult with 3 parameters
 # add num1 and num2, multiply the result with num3
 # return the final result
 
-def add_mult(num1, num2, num3):
+def add_mult(user_expression):
     """ Return the sum of num 1 + num2 multiplied by num3"""
-    equation = (num1 + num2) * num3
+  
+
+    num = convert_to_int(user_expression)
+    equation = (num[0] + num[1]) * num[2]
     return equation 
 
 #define function add_cubes with 2 parameters
 #define vaiable that takes the cubes of num1 and num2 and adds them together
 #return the sum of the cubes save in that variable
 
-def add_cubes(num1, num2) :
+def add_cubes(user_expression) :
     """ Return the sum of cubes."""
-    sum_cubes = (num1**3) + (num2**3)
+    num = convert_to_int(user_expression)
+    sum_cubes = (num[0]**3) + (num[1]**3)
     return sum_cubes
--- calculator.py
+++ calculator.py
@@ -21,48 +21,36 @@ while True:
     user_expression = input("Enter your equation!")
     if user_expression != "q":
         tokenized_input = user_expression.split(' ')
-        num1 = int(tokenized_input[1])
-       
-        if len(tokenized_input) == 2:
-            num1 = int(tokenized_input[1])
-        elif len(tokenized_input) == 3:
-            num1 = int(tokenized_input[1])
-            num2 = int(tokenized_input[2])
-        if len(tokenized_input) == 4:
-            num1 = int(tokenized_input[1])
-            num2 = int(tokenized_input[2])
-            num3 = int(tokenized_input[3])   
-        
 
         if tokenized_input[0] == "+" :
-            result = add(num1,num2)
+            result = add(tokenized_input)
 
         if tokenized_input[0] == "-" :
-            result = subtract(num1,num2)
+            result = subtract(tokenized_input)
 
         if tokenized_input[0] == "*":
-            result = multiply(num1,num2)
+            result = multiply(tokenized_input)
         
         if tokenized_input[0] == "/":
-            result = divide(num1,num2)
+            result = divide(tokenized_input)
         
         if tokenized_input[0] == "%":
-            result = mod(num1,num2)
+            result = mod(tokenized_input)
 
         if tokenized_input[0] == "squared":
-            result = square(num1)
+            result = square(tokenized_input)
         
         if tokenized_input[0] == "cubed":
-            result = cube(num1)
+            result = cube(tokenized_input)
 
         if tokenized_input[0] == "power":
-            result = power(num1,num2)
+            result = power(tokenized_input)
         
         if tokenized_input[0] == "add_prod":
-            result = add_mult(num1,num2,num3)
+            result = add_mult(tokenized_input)
 
         if tokenized_input[0] == "sum_cubes":
-            result = add_cubes(num1,num2)
+            result = add_cubes(tokenized_input)
         
     elif user_expression =="q":
         break
